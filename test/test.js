a10dd2515ee62fe7156083f78b62e72335d92f2e
jerry-weng <jjweng2007@gmail.com>
2016-06-22 18:35:41 +0800

    fix test

--- test/test.js
+++ test/test.js
@@ -1,38 +1,49 @@
 'use strict';
 
+var Level = require('gpio').Level;
 var assert = require('assert');
-var path = require('path');
+
 var mock = require('ruff-mock');
 
 var when = mock.when;
 var any = mock.any;
+var mockAny = mock.mockAny;
 
-var driverPath = path.join(__dirname, '..');
-var runner = require('ruff-driver-runner');
+var Device = require('../');
 
 require('t');
 
-describe('Driver for Relay', function () {
+describe('Relay Driver', function () {
     var relay;
     var gpio;
 
-    before(function (done) {
-        runner.run(driverPath, function (device, context) {
-            relay = device;
-            gpio = context.arg('gpio');
-            done();
+    before(function () {
+        gpio = mockAny();
+        relay = new Device({
+            gpio: gpio
         });
     });
 
-    it('shoule gpio write 0 when realy turn on', function (done) {
-        when(gpio).write(0, callback).then(function () {
-            callback();
-        });
-        relay.turnOn(callback);
+    it('shoule gpio write `Level.low` when turn on', function (done) {
+        when(gpio)
+            .write(any, any)
+            .then(function (level, callback) {
+                assert.equal(level, Level.low);
+                callback();
+            });
 
-        function callback() {
-            done();
-        }
+        relay.turnOn(done);
+    });
+
+    it('shoule gpio write `Level.high` when turn off', function (done) {
+        when(gpio)
+            .write(any, any)
+            .then(function (level, callback) {
+                assert.equal(level, Level.high);
+                callback();
+            });
+
+        relay.turnOff(done);
     });
 
     it('shoule gpio write 1 when realy turn off', function (done) {
@@ -46,16 +57,17 @@ describe('Driver for Relay', function () {
         }
     });
 
-    it('should isOn when gpio read 0', function (done) {
-        when(gpio).read(any()).then(function () {
-            callback(undefined, true);
-        });
-        relay.isOn(callback);
+    it('should have `isOn` called back with true when gpio read `Level.high`', function (done) {
+        when(gpio)
+            .read(any)
+            .then(function (callback) {
+                callback(undefined, Level.low);
+            });
 
-        function callback(error, on) {
-            assert(!error);
-            assert(on);
+        relay.isOn(function (error, on) {
+            assert.ifError(error);
+            assert.strictEqual(on, true);
             done();
-        }
+        });
     });
 });
